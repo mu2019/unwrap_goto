04393db1b33a938a181b747607593db5b6b6866b
sheng <kimoo@163.com>
2022-07-29 14:05:26 +0800

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,142 @@
+
+#[macro_export]
+macro_rules! some_or_continue{
+    ($e:expr) => {
+        {
+            if $e.is_none() {
+                continue;
+            } else {
+                $e.unwrap()
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! some_or_break{
+    ($e:expr) => {
+        {
+            if $e.is_none() {
+                break;
+            } else {
+                $e.unwrap()
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! some_or_return {
+    ($e:expr) => {
+        {
+            if $e.is_none() {
+                return;
+            } else {
+                $e.unwrap()
+            }
+        }
+    };
+    ($e:expr, $r: expr) => {
+        {
+            if $e.is_none() {
+                return $r;
+            } else {
+                $e.unwrap()
+            }
+        }
+    };    
+}
+
+#[macro_export]
+macro_rules! ok_or_continue{
+    ($e:expr) => {
+        {
+            if $e.is_err() {
+                continue;
+            } else {
+                $e.unwrap()
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! ok_or_return{
+    ($e:expr) => {
+        {
+            if $e.is_err() {
+                return;
+            } else {
+                $e.unwrap()
+            }
+        }
+    };
+    ($e:expr, $r: expr) => {
+        {
+            if $e.is_err() {
+                return $r;
+            } else {
+                $e.unwrap()
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! ok_or_break{
+    ($e:expr) => {
+        {
+            if $e.is_err() {
+                break;
+            } else {
+                $e.unwrap()
+            }
+        }
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn none_is_continue() {
+        let m = vec![Some(1), None, Some(2)];
+        let mut k = 0;
+        for j in m {
+            k = some_or_continue!(j);
+        }
+        assert_eq!(k, 2);        
+    }
+
+    #[test]
+    fn none_is_break() {
+        let m = vec![Some(1), None, Some(2)];
+        let mut k = 0;
+        for j in m {
+            println!("j:{:?}", j);
+            k = some_or_break!(j);
+            println!("k:{:?}", k);            
+        }
+        println!("k2:{:?}", k);
+        assert_eq!(k, 1);
+    }
+
+    fn do_none_is_return(m: &Vec<Option<i32>>) -> i64 {
+        let mut k = 0;
+        for v in m {
+            println!("v:{:?}", v);
+            k = some_or_return!(v, 8);
+            println!("k:{:?}", k);
+        }
+        assert_eq!(k, 1);
+        0
+    }
+
+    #[test]
+    fn none_is_return() {
+        let m = vec![Some(1), None, Some(1)];
+        let n = do_none_is_return(&m);
+        println!("n:{:?}", n);
+
+    }    
+    
+}
